a00261623c180b7e66f88eaaeef71035deae60bf
Stephen Bodnar <anapaix@Apple-Macintosh-2.local>
2018-05-19 10:39:17 +0700

    begin adding comment box

--- Sources/App/Entities/Structs/PostStructs.swift
+++ Sources/App/Entities/Structs/PostStructs.swift
@@ -22,6 +22,19 @@ extension Post {
     }
 }
 
+struct FormattedTutorialPost: Content {
+    var id:Int?
+    var postTitle: String
+    let user_id: Int
+    var viewCount: Int
+    var isTutorial: Bool
+    var tutorialType: String
+    var isPublished: Bool
+    var vaporVersion: Int
+    var humanDate: String
+    var synHiContent: String
+}
+
 struct PostWithoutContent: Content  {
     var id:Int?
     var postTitle: String
